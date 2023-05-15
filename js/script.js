8c02ed687a1cd392f5e562159828e50e483cdeb4
kervines <kervinzero@gmail.com>
2023-05-14 23:48:41 -0300

    final commit: inclusao da funcionalidade de dia e noite

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const hours = document.querySelector('.hours');
+const minutes = document.querySelector('.minutes');
+const seconds = document.querySelector('.seconds');
+const ampm = document.querySelector('.am-pm');
+const body = document.body;
+
+setInterval(() => {
+  const data = new Date();
+
+  let h = data.getHours();
+  let m = data.getMinutes();
+  let s = data.getSeconds();
+
+  h < 10 ? (hours.innerHTML = '0' + h) : (hours.innerHTML = h);
+  m < 10 ? (minutes.innerHTML = '0' + m) : (minutes.innerHTML = m);
+  s < 10 ? (seconds.innerHTML = '0' + s) : (seconds.innerHTML = s);
+
+  if (h >= 18) {
+    body.classList.add('night');
+    ampm.innerHTML = 'PM';
+  } else {
+    body.classList.remove('night');
+    ampm.innerHTML = 'AM';
+  }
+}, 1000);
